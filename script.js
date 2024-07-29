333370c4bfbbe83cef241f1df1a680ec1d767e84
hamza-elouadi <hamzaelouadi2004@gmail.com>
2024-07-29 12:47:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var i = 0;
+var images = [];
+var time = 4000;
+
+images[0] = '187818.jpg';
+images[1] = '187819.jpg';
+images[2] = '187820.jpg';
+images[3] = '187821.jpg';
+images[4] = '187822.jpg';
+
+function changimg(){
+    document.slide.src = images[i];
+
+    if(i<images.length - 1){
+        i++;
+    }else{
+        i = 0;
+    }
+    setTimeout("changimg()" , time);
+}
+window.onload = changimg;
